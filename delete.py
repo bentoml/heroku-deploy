a4e3b3601a7ccd484f8ee529f73541b1cda08eb2
Damir Temir <dtemi2@uis.edu>
2021-11-11 14:37:55 -0600

    refactor: fix argument

--- delete.py
+++ delete.py
@@ -4,7 +4,7 @@ import argparse
 from .utils import run_shell_command, generate_heroku_app_name
 
 
-def delete(deployment_name, heroku_config={}):
+def delete(deployment_name, heroku_config=None):
     app_name = generate_heroku_app_name(deployment_name)
     print(f"Removing app {app_name}")
     run_shell_command(["heroku", "apps:destroy", app_name, "--confirm", app_name])
--- describe.py
+++ describe.py
@@ -4,7 +4,7 @@ import argparse
 from .utils import generate_heroku_app_name, run_shell_command
 
 
-def describe(deployment_name, heroku_config={}):
+def describe(deployment_name, heroku_config=None):
     app_name = generate_heroku_app_name(deployment_name)
     stdout, stderr = run_shell_command(['heroku', 'apps:info', '--app', app_name])
     print(stdout)
