fd4d79e380e88f0467f0d4f7943dec6e48aafe58
jjmachan <jamesjithin97@gmail.com>
2022-05-23 23:44:18 +0530

    remove old files

--- heroku_deploy/describe.py
+++
@@ -1,7 +0,0 @@
-from .utils import generate_heroku_resource_names, run_shell_command
-
-
-def describe(deployment_name, deployment_spec=None):
-    app_name, _ = generate_heroku_resource_names(deployment_name)
-    stdout, stderr = run_shell_command(["heroku", "apps:info", "--app", app_name])
-    print(stdout)
