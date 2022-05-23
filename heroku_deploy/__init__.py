acda9d4499d77b825ccdc2fb567ca6ed28517801
jjmachan <jamesjithin97@gmail.com>
2022-05-23 21:41:50 +0530

    new generate and repository commands

--- heroku_deploy/__init__.py
+++ heroku_deploy/__init__.py
@@ -1,12 +1,8 @@
-import os
-
-from bentoctl.utils.operator_helpers import Generate
 from bentoctl.utils.operator_helpers import (
     create_deployable_from_local_bentostore as create_deployable,
 )
 
+from heroku_deploy.generate import generate
 from heroku_deploy.registry_utils import create_repository, delete_repository
-
-generate = Generate(os.path.join(os.path.dirname(__file__), "templates"))
 
 __all__ = ["generate", "create_deployable", "create_repository", "delete_repository"]
